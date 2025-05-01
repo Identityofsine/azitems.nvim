34eb049b02e0ba57876ceafb61094861b3e1b578
Erdogan, Kevin <kerdogan@lbisoftware.com>
2025-05-01 09:35:07 -0400

    added rendering logic for preview

--- lua/azitems.lua
+++ lua/azitems.lua
@@ -2,6 +2,7 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local actions = require("telescope.actions")
+local previewers = require("telescope.previewers")
 local action_state = require("telescope.actions.state")
 local entry_display = require("telescope.pickers.entry_display")
 
@@ -21,6 +22,30 @@ M.hello = function()
   return module.my_first_function(config.opt)
 end
 
+local displayer = entry_display.create({
+  separator = " ",
+  items = {
+    { width = 10 },
+    { width = 60 },
+  },
+})
+
+local previewer = previewers.new_buffer_previewer({
+	define_preview = function(self, entry, status)
+		local workItem = entry.value
+		local preview_text = string.format(
+			"ID: %d\nTitle: %s\nAssigned To: %s\nState: %s\nDescription: %s",
+			workItem.id,
+			workItem.fields.title,
+			workItem.fields.assignedTo.displayName,
+			workItem.fields.state,
+			workItem.fields.description
+		)
+		self.state.bufnr = self.state.bufnr or vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(preview_text, "\n"))
+	end,
+})
+
 M.workItems = function()
 	local workItems = module.getWorkItems()
 	pickers.new({}, {
@@ -32,12 +57,16 @@ M.workItems = function()
 				return {
 					value = entry,
 					display = function()
-						return workitemUtils[entry.fields.workItemType](string.format("%s — %s", entry.id, entry.fields.title))
+						return displayer({
+							{ workitemUtils[entry.fields.workItemType](tostring(entry.id)), "Error" },
+							{ entry.fields.title, "Error" },
+						})
 					end,
 					ordinal = entry.id .. " " .. entry.fields.title,
 				}
 			end,
 		},
+		previewer = previewer,
 		sorter = conf.generic_sorter({}),
 		attach_mappings = function(prompt_bufnr, map)
 			actions.select_default:replace(function()
